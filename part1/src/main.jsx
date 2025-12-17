1a139777073bd0aee17362ec71d185eeb260d641
Antti Partinen <antti.partinen@student.lab.fi>
2025-12-17 10:46:33 +0200

    Tehty tehtävät 1.1 ja 1.2

---
+++ part1/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+// Renderöidään App-komponentti elementtiin, jonka id on 'root'
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
